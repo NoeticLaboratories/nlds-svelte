37b082aea10a37b7feb160e674b759f55375de59
Marco De Santis <marcodsn.bit@gmail.com>
2025-04-16 19:02:45 +0000

    Fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,10 @@ const config = {
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
             out: "build",
+            precompress: true,
+            env: {
+                port: "PORT",
+            },
         }),
     },
 };
