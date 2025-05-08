d2265ff49fb8d26fb0a8fd6b59c4f972659abd0c
Marco De Santis <marcodsn.bit@gmail.com>
2025-05-08 12:22:46 +0200

    Add Select component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from "./components/Button/Button.svelte";
 export { default as Card } from "./components/Card/Card.svelte";
 export { default as CodeSnippet } from "./components/CodeSnippet/CodeSnippet.svelte";
 export { default as NumberInput } from "./components/NumberInput/NumberInput.svelte";
+export { default as Select } from "./components/Select/Select.svelte";
 
 export { default as StructuredList } from "./components/StructuredList/StructuredList.svelte";
 export { default as StructuredListRow } from "./components/StructuredList/StructuredListRow.svelte";
